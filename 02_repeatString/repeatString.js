1ef52ef58a46fc0950aaae91bb2a9cacc3fe6ea3
Fiza Hussain <hussainfiza5@gmail.com>
2023-07-25 22:51:12 +0500

    ex 1-7 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, n) {
+    let string = "";
+    if (n<0) {
+        return "ERROR";
+    }
+    for (let i=0; i<n; i++) {
+        string+=word;
+    }
+    return string
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let a;
+    let b;
+    let f = [90, 1];
+    let x = typeof f;
+    if ((typeof num1 != "number") || (typeof num2 != "number") || (num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+    if (num1 > num2) {
+        a = num2;
+        b = num1;
+    }
+    else {
+        a = num1;
+        b = num2;
+    }
+    let sum = 0;
+    for(let i=a; i<=b; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
