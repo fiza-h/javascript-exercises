397a944984b3a14f5e3f2aea51b043143b5cb305
Fiza Hussain <hussainfiza57@gmail.com>
2023-07-30 13:20:35 +0500

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,6 @@
-const findTheOldest = function(Arr) {
+const findTheOldest = function(people) {
     const currentDate = new Date();
-    const oldestPerson = Arr.reduce((oldest, current) => {
+    const oldestPerson = people.reduce((oldest, current) => {
     const oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : currentDate.getFullYear() - oldest.yearOfBirth;
     const currentAge = current.yearOfDeath ? current.yearOfDeath - current.yearOfBirth : currentDate.getFullYear() - current.yearOfBirth;
     return currentAge > oldestAge ? current : oldest;
